d4499edb9bb461f8888f61d76b3ee9b0a7626120
Foxdim <103509067+Foxdim@users.noreply.github.com>
2023-01-09 15:18:47 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+/**/
+include_once("Module/TxtCache.php");
+$Fox_Cache = new Fox_Cache_Module();
+$Fox_Cache->prefix="Prefix";
+
+/**/
+$cache_time=5;
+$cache_name="HelloWorld";
+
+
+/*Page cache ussage*/
+echo "<h3>Test Page Cache</h3><br>";
+$page_cache=$Fox_Cache->Page_Cache("start",$cache_name,$cache_time,"false","get");
+if($page_cache["status"]=="success")echo $page_cache["value"];
+else if($page_cache["status"]!="success")
+{
+/*Your codes.*/
+echo date("Y-m-d H:i:s",time());
+/*Stop cache.*/
+$Fox_Cache->Page_Cache("fin",$cache_name,$cache_time,"false","post");
+}
+
+echo "<br><hr><br>";
+echo "<h3>Test Delegate Cache</h3><br>";
+$return_data_delegate=$Fox_Cache->delegate(function(){ 
+    echo date("Y-m-d H:i:s",time());
+    //work return params $return_data_delegate
+},$cache_name,$cache_time,"false");
+
+
+
+
+?>
