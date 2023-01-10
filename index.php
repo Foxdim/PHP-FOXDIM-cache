e831f87e0248e68f0300651c7cfa2e35cd871de2
Foxdim <103509067+Foxdim@users.noreply.github.com>
2023-01-10 12:59:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,18 @@ $Fox_Cache->prefix="Prefix";
 /**/
 $cache_time=5;
 $cache_name="HelloWorld";
-
+$content_type="text/html; charset=UTF-8";
 
 /*Page cache ussage*/
 echo "<h3>Test Page Cache</h3><br>";
-$page_cache=$Fox_Cache->Page_Cache("start",$cache_name,$cache_time,"false","get");
+$page_cache=$Fox_Cache->Page_Cache("start",$cache_name,$cache_time,false,"post",$content_type);
 if($page_cache["status"]=="success")echo $page_cache["value"];
 else if($page_cache["status"]!="success")
 {
 /*Your codes.*/
 echo date("Y-m-d H:i:s",time());
 /*Stop cache.*/
-$Fox_Cache->Page_Cache("fin",$cache_name,$cache_time,"false","post");
+$Fox_Cache->Page_Cache("fin",$cache_name,$cache_time,false,"post",$content_type);
 }
 
 echo "<br><hr><br>";
@@ -26,7 +26,7 @@ echo "<h3>Test Delegate Cache</h3><br>";
 $return_data_delegate=$Fox_Cache->delegate(function(){ 
     echo date("Y-m-d H:i:s",time());
     //work return params $return_data_delegate
-},$cache_name,$cache_time,"false");
+},$cache_name,$cache_time,false,$content_type);
 
 
 
